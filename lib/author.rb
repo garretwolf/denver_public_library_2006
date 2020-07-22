e8b06a850496cdbe9ea6533d14ef00621e9a34bd
Garret Wolf <garretwolf26@gmail.com>
2020-07-22 15:19:47 -0400

    Add code to write method to create an instance of Book

--- lib/author.rb
+++ lib/author.rb
@@ -12,6 +12,8 @@ class Author
   end
 
   def write(title, publication_date)
-    require "pry"; binding.pry
+    #creates book
+    Book.new({:author_first_name => @first_name, :author_last_name => @last_name,
+      :title => title, :publication_date => publication_date})
   end
 end
