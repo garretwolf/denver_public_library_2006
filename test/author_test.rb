f76ba1a5b1b734dc74885436e89a73acccd874b6
Garret Wolf <garretwolf26@gmail.com>
2020-07-22 14:57:49 -0400

    Fix author test to be more specific

--- test/author_test.rb
+++ test/author_test.rb
@@ -11,10 +11,15 @@ class AuthorTest < Minitest::Test
     assert_instance_of Author, charlotte_bronte
   end
 
-  def test_it_has_attributes
+  def test_it_has_name
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
 
     assert_equal "Charlotte Bronte", charlotte_bronte.name
+  end
+
+  def test_books_starts_empty
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
     assert_equal [], charlotte_bronte.books
   end
 end
