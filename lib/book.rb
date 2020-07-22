5998b622d5fd5df6f800a450347289dc1158e872
Garret Wolf <garretwolf26@gmail.com>
2020-07-22 14:44:00 -0400

    Create author method to combine first and last names

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,15 @@
 class Book
+  attr_reader :title, :author_first_name, :author_last_name,
+  :publication_date
 
   def initialize(info)
     @author_first_name = info[:author_first_name]
-    @author_last_name = [:author_last_name]
+    @author_last_name = info[:author_last_name]
     @title = info[:title]
     @publication_date = info[:publication_date]
+  end
+
+  def author
+    "#{author_first_name} " + "#{author_last_name}"
   end
 end
