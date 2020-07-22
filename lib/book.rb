fc60b207ccc4809cc970a1e63dfde6c61fa83829
Garret Wolf <garretwolf26@gmail.com>
2020-07-22 14:39:43 -0400

    Build initialize method

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,10 @@
 class Book
+
+
+  def initialize(info)
+    @author_first_name = info[:author_first_name]
+    @author_last_name = [:author_last_name]
+    @title = [:title]
+    @publication_date = [:publication_date]
+  end
 end
