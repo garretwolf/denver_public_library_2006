7df4da351c7979ecb5120544fb83523681899b9f
Garret Wolf <garretwolf26@gmail.com>
2020-07-22 16:10:38 -0400

    Create books method and finish test for finding books

--- lib/library.rb
+++ lib/library.rb
@@ -9,5 +9,12 @@ class Library
 
   def add_author(author)
     @authors << author
+    # require "pry"; binding.pry
+  end
+
+  def books
+    @authors.map do |author|
+      author.books
+    end.flatten
   end
 end
