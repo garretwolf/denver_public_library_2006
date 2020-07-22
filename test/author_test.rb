9fd8b4591abdffec7262c5e5b2d311bc28e58590
Garret Wolf <garretwolf26@gmail.com>
2020-07-22 15:37:55 -0400

    Fix test it can return array of books

--- test/author_test.rb
+++ test/author_test.rb
@@ -29,5 +29,13 @@ class AuthorTest < Minitest::Test
 
     assert_equal Book, jane_eyre.class
     assert_equal "Jane Eyre", jane_eyre.title
-  end  
+  end
+
+  def test_it_can_return_array_of_books
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    assert_equal [jane_eyre, villette], charlotte_bronte.books
+  end
 end
