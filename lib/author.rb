d5898c33d2726fc363d65421086dc92b795913b3
Garret Wolf <garretwolf26@gmail.com>
2020-07-22 14:56:18 -0400

    Add initialize method and create name method

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,12 @@
-class Author 
+class Author
+  attr_reader :first_name, :last_name
 
+  def initialize(name)
+    @first_name = name[:first_name]
+    @last_name = name[:last_name]
+  end
 
+  def name
+    "#{first_name} " + "#{last_name}"
+  end
 end
