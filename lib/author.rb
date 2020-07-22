ef127d40241cb8a1630c005781f4b018b719305e
Garret Wolf <garretwolf26@gmail.com>
2020-07-22 14:59:18 -0400

    Create books method and initialize as empty array

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,10 @@
 class Author
-  attr_reader :first_name, :last_name
+  attr_reader :first_name, :last_name, :books
 
   def initialize(name)
     @first_name = name[:first_name]
     @last_name = name[:last_name]
+    @books = []
   end
 
   def name
